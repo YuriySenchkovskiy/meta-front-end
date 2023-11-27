5f6caf0ec8d0040731aca108878dce0cda8aa4d4
Yuriy <yuriy.senchkovskiy@gmail.com>
2023-11-27 09:41:51 -0500

    Add html/css final project - little lemon

--- Programming with JS/Practice.js
+++ Programming with JS/Practice.js
@@ -1,11 +1,25 @@
-function isOpposite(s1,s2){
-    if (s1.length === 0 || s1.length !== s2.length) return false;
+function findEvenIndex(arr) {
+    let index = 0;
 
-    for (var i = 0; i < s1.length; i++) {
-        if(s1[i] === s2[i]) return false;
+    for (let i = 0; i < arr.length; i++) {
+
+        let sumRight = 0;
+        let sumLeft = 0;
+
+        for (let j = 0; j < i; j++) {
+            sumRight += arr[j];
+        }
+
+        for (let j = i + 1; j < arr.length; j++) {
+            sumLeft += arr[j];
+        }
+
+        if (sumRight === sumLeft) {
+            return i;
+        }
     }
 
-    return true;
+    return -1;
 }
 
-console.log(isOpposite("aBcde","AbCD"))
+console.log(findEvenIndex([1,2,3,4,3,2,1]));
